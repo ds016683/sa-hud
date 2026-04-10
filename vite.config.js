2458e7a586969ad8617cb7ca750fa3bdfb8a2aef
Mr. Build <david.smith@thirdhorizon.com>
2026-04-10 16:28:02 -0500

    fix: force cache-bust on every deploy via timestamp in asset filenames

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sa-hud/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: ssets/[name]-[hash]-.js,
+        chunkFileNames: ssets/[name]-[hash]-.js,
+        assetFileNames: ssets/[name]-[hash]-.[ext],
+      }
+    }
+  }
 })
